81e71c06c93b95fb9cadb42f3393a8f22ded7509
phucdo2601 <ngocphucdo2601@gmail.com>
2026-06-30 22:22:02 +0700

    [learn-vuejs-nuxtjs-fun-b01] 02. Basic create routing on project b01

---
+++ learn-fun-nuxtjs-b01/app/components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <ul>
+      <li><strong>Tasks App</strong></li>
+    </ul>
+    <ul>
+      <li>
+        <NuxtLink :to="{
+            name: 'create'
+        }"> Create Task </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
--- learn-fun-nuxtjs-b01/nuxt.config.ts
+++ learn-fun-nuxtjs-b01/nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+	modules: ["@nuxt/eslint"],
+	devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	css: [
+		"@picocss/pico"
+	],
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
