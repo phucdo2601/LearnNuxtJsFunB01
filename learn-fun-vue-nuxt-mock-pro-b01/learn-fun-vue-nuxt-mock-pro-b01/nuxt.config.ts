6c655250bae50d50c865df16fb730cb08c4818de
phucdn <phucdn@allexceed.co.jp>
2026-07-02 11:27:51 +0700

    [learn-fun-vue-nuxt-mock-pro-b01] 01. Basic create learn-fun-vue-nuxt-mock-pro-b01 project structure b01

--- learn-fun-vue-nuxt-mock-pro-b01/learn-fun-vue-nuxt-mock-pro-b01/nuxt.config.ts
+++ learn-fun-vue-nuxt-mock-pro-b01/learn-fun-vue-nuxt-mock-pro-b01/nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ]
 })
