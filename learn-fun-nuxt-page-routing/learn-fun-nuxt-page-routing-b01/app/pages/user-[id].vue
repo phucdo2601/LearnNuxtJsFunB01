b4a4ba7f94575c4348d3bbb83b4714f1111bde44
phucdn <phucdn@allexceed.co.jp>
2026-07-06 11:12:52 +0700

    [learn-nuxt-routing-page-b01] [learn-pro-b01] 02. Basic learn  Basic Dynamic Route b01

---
+++ learn-fun-nuxt-page-routing/learn-fun-nuxt-page-routing-b01/app/pages/user-[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Dynamic route page!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
